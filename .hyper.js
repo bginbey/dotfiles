c5fe5653e2bef2a91cb3bcaf287f671c439d26c9
Beau Ginbey <bginbey@gmail.com>
2019-06-14 23:08:41 +0800

    Add search

--- .hyper.js
+++ .hyper.js
@@ -146,7 +146,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ["hyperlinks", "hyper-solarized-dark"],
+  plugins: ["hyperlinks", "hyper-solarized-dark", "hyper-search"],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
